908f53e1485ed02102703c7d038ef7d47635de45
amrmorsey <amr.morsey@aucegypt.edu>
2017-09-06 01:08:20 +0200

    Used std::inner_product() instead of dotNoSSE()

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <exception>
+#include <numeric>
 #include "Matrix.h"
 
 
@@ -161,7 +162,8 @@ Matrix Matrix::dot(Matrix *filter, int x) {
             vector<float> a(first1, last1);
             //VecAVX v(X_col_shape.at(0), a);
 //            float res = v.dot(w);
-            float res = dotNoSSE(a, b);
+            //float res = dotNoSSE(a, b); // can either do this
+            float res = float(std::inner_product(a.begin(), a.end(), b.begin(), 0.0)); //or this
             out.matrix.at(index) = res;
             x_dim += X_col_shape.at(0);
             index++;
@@ -328,48 +330,6 @@ void Matrix::subNoSSE(Matrix &m) {
         this->matrix[i] = this->matrix[i] - average;
     }
 }
-
-//void im2col_cpu(const Dtype* data_im, const int channels,
-//                const int height, const int width, const int kernel_h, const int kernel_w,
-//                const int pad_h, const int pad_w,
-//                const int stride_h, const int stride_w,
-//                const int dilation_h, const int dilation_w,
-//                Dtype* data_col) {
-//    const int output_h = (height + 2 * pad_h -
-//                          (dilation_h * (kernel_h - 1) + 1)) / stride_h + 1;
-//    const int output_w = (width + 2 * pad_w -
-//                          (dilation_w * (kernel_w - 1) + 1)) / stride_w + 1;
-//    const int channel_size = height * width;
-//    for (int channel = channels; channel--; data_im += channel_size) {
-//        for (int kernel_row = 0; kernel_row < kernel_h; kernel_row++) {
-//            for (int kernel_col = 0; kernel_col < kernel_w; kernel_col++) {
-//                int input_row = -pad_h + kernel_row * dilation_h;
-//                for (int output_rows = output_h; output_rows; output_rows--) {
-//                    if (!is_a_ge_zero_and_a_lt_b(input_row, height)) {
-//                        for (int output_cols = output_w; output_cols; output_cols--) {
-//                            *(data_col++) = 0;
-//                        }
-//                    } else {
-//                        int input_col = -pad_w + kernel_col * dilation_w;
-//                        for (int output_col = output_w; output_col; output_col--) {
-//                            if (is_a_ge_zero_and_a_lt_b(input_col, width)) {
-//                                *(data_col++) = data_im[input_row * width + input_col];
-//                            } else {
-//                                *(data_col++) = 0;
-//                            }
-//                            input_col += stride_w;
-//                        }
-//                    }
-//                    input_row += stride_h;
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//inline bool is_a_ge_zero_and_a_lt_b(int a, int b) {
-//    return static_cast<unsigned>(a) < static_cast<unsigned>(b);
-//}
 
 Matrix Matrix::maxPooling(int kernel_size, int stride, int padding) {
     int pad = padding;

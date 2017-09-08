def37bf0e20b6c93c60de244370718270cf5c631
Shady Fanous <shady-fanous@aucgypt.edu>
2017-09-08 17:36:49 +0200

    MatrixAVX class now supports dot, add and sub operations

--- src/MatrixAVX.cpp
+++ src/MatrixAVX.cpp
@@ -5,30 +5,12 @@
 #include "MatrixAVX.h"
 
 
-inline void transpose4x4_SSE(float *A, float *B, const int lda, const int ldb) {
-    __m128 row1 = _mm_load_ps(&A[0 * lda]);
-    __m128 row2 = _mm_load_ps(&A[1 * lda]);
-    __m128 row3 = _mm_load_ps(&A[2 * lda]);
-    __m128 row4 = _mm_load_ps(&A[3 * lda]);
-    _MM_TRANSPOSE4_PS(row1, row2, row3, row4);
-    _mm_store_ps(&B[0 * ldb], row1);
-    _mm_store_ps(&B[1 * ldb], row2);
-    _mm_store_ps(&B[2 * ldb], row3);
-    _mm_store_ps(&B[3 * ldb], row4);
-}
-
-inline void transpose_block_SSE4x4(float *A, float *B, const int n, const int m, const int lda, const int ldb,
-                                   const int block_size) {
-#pragma omp parallel for
-    for (int i = 0; i < n; i += block_size) {
-        for (int j = 0; j < m; j += block_size) {
-            int max_i2 = i + block_size < n ? i + block_size : n;
-            int max_j2 = j + block_size < m ? j + block_size : m;
-            for (int i2 = i; i2 < max_i2; i2 += 4) {
-                for (int j2 = j; j2 < max_j2; j2 += 4) {
-                    transpose4x4_SSE(&A[i2 * lda + j2], &B[j2 * ldb + i2], lda, ldb);
-                }
-            }
-        }
-    }
+//    https://stackoverflow.com/questions/13879609/horizontal-sum-of-8-packed-32bit-floats
+__m256 MatrixAVX::hsums(__m256 const &v) {
+    auto x = _mm256_permute2f128_ps(v, v, 1);
+    auto y = _mm256_add_ps(v, x);
+    x = _mm256_shuffle_ps(y, y, _MM_SHUFFLE(2, 3, 0, 1));
+    x = _mm256_add_ps(x, y);
+    y = _mm256_shuffle_ps(x, x, _MM_SHUFFLE(1, 0, 3, 2));
+    return _mm256_add_ps(x, y);
 }
--- src/MatrixAVX.h
+++ src/MatrixAVX.h
@@ -1,158 +1,28 @@
-//
-// Created by shadyf on 07/09/17.
-//
-
 #ifndef INFERENCEENGINE_MATRIXAVX_H
 #define INFERENCEENGINE_MATRIXAVX_H
 
 #ifdef _WIN32
 #include <malloc.h>
 #endif
+
+#include "AlignedVector.h"
+
 #include <avxintrin.h>
 #include <xmmintrin.h>
 #include <vector>
 #include <cmath>
-
-template <typename T, std::size_t Alignment>
-class aligned_allocator
-{
-public:
-
-    // The following will be the same for virtually all allocators.
-    typedef T * pointer;
-    typedef const T * const_pointer;
-    typedef T& reference;
-    typedef const T& const_reference;
-    typedef T value_type;
-    typedef std::size_t size_type;
-    typedef ptrdiff_t difference_type;
-
-    T * address(T& r) const
-    {
-        return &r;
-    }
-
-    const T * address(const T& s) const
-    {
-        return &s;
-    }
-
-    std::size_t max_size() const
-    {
-        // The following has been carefully written to be independent of
-        // the definition of size_t and to avoid signed/unsigned warnings.
-        return (static_cast<std::size_t>(0) - static_cast<std::size_t>(1)) / sizeof(T);
-    }
-
-
-    // The following must be the same for all allocators.
-    template <typename U>
-    struct rebind
-    {
-        typedef aligned_allocator<U, Alignment> other;
-    } ;
-
-    bool operator!=(const aligned_allocator& other) const
-    {
-        return !(*this == other);
-    }
-
-    void construct(T * const p, const T& t) const
-    {
-        void * const pv = static_cast<void *>(p);
-
-        new (pv) T(t);
-    }
-
-    void destroy(T * const p) const
-    {
-        p->~T();
-    }
-
-    // Returns true if and only if storage allocated from *this
-    // can be deallocated from other, and vice versa.
-    // Always returns true for stateless allocators.
-    bool operator==(const aligned_allocator& other) const
-    {
-        return true;
-    }
-
-
-    // Default constructor, copy constructor, rebinding constructor, and destructor.
-    // Empty for stateless allocators.
-    aligned_allocator() { }
-
-    aligned_allocator(const aligned_allocator&) { }
-
-    template <typename U> aligned_allocator(const aligned_allocator<U, Alignment>&) { }
-
-    ~aligned_allocator() { }
-
-
-    // The following will be different for each allocator.
-    T * allocate(const std::size_t n) const
-    {
-        // The return value of allocate(0) is unspecified.
-        // Mallocator returns NULL in order to avoid depending
-        // on malloc(0)'s implementation-defined behavior
-        // (the implementation can define malloc(0) to return NULL,
-        // in which case the bad_alloc check below would fire).
-        // All allocators can return NULL in this case.
-        if (n == 0) {
-            return NULL;
-        }
-
-        // All allocators should contain an integer overflow check.
-        // The Standardization Committee recommends that std::length_error
-        // be thrown in the case of integer overflow.
-        if (n > max_size())
-        {
-            throw std::length_error("aligned_allocator<T>::allocate() - Integer overflow.");
-        }
-
-        // Mallocator wraps malloc().
-        void * const pv = _mm_malloc(n * sizeof(T), Alignment);
-
-        // Allocators should throw std::bad_alloc in the case of memory allocation failure.
-        if (pv == NULL)
-        {
-            throw std::bad_alloc();
-        }
-
-        return static_cast<T *>(pv);
-    }
-
-    void deallocate(T * const p, const std::size_t n) const
-    {
-        _mm_free(p);
-    }
-
-
-    // The following will be the same for all allocators that ignore hints.
-    template <typename U>
-    T * allocate(const std::size_t n, const U * /* const hint */) const
-    {
-        return allocate(n);
-    }
-
-
-    // Allocators are not required to be assignable, so
-    // all allocators should have a private unimplemented
-    // assignment operator. Note that this will trigger the
-    // off-by-default (enabled under /Wall) warning C4626
-    // "assignment operator could not be generated because a
-    // base class assignment operator is inaccessible" within
-    // the STL headers, but that warning is useless.
-private:
-    aligned_allocator& operator=(const aligned_allocator&);
-};
-
-typedef std::vector<__m256, aligned_allocator<__m256, sizeof(__m256)> > aligned_vector;
+#include <exception>
+#include <string>
+#include <algorithm>
 
 class MatrixAVX {
 private:
+    aligned_vector xmm;
+    unsigned long xmm_size;
+    unsigned long aligned_size;
+    unsigned int stranglers;
+    __attribute__((aligned(sizeof(__m256)))) float aligned_float_arr[8];
 
-//    https://stackoverflow.com/questions/13879609/horizontal-sum-of-8-packed-32bit-floats
     static inline __m256 hsums(__m256 const &v) {
         auto x = _mm256_permute2f128_ps(v, v, 1);
         auto y = _mm256_add_ps(v, x);
@@ -160,15 +30,13 @@ private:
         x = _mm256_add_ps(x, y);
         y = _mm256_shuffle_ps(x, x, _MM_SHUFFLE(1, 0, 3, 2));
         return _mm256_add_ps(x, y);
-    }
+    };
 
-    aligned_vector xmm;
+public:
     unsigned long matrix_size;
     std::vector<int> matrix_shape;
-    unsigned long xmm_size;
-public:
 
-    explicit MatrixAVX(std::vector<float> vec, std::vector<int> shape) : matrix_shape(shape){
+    explicit MatrixAVX(std::vector<float> vec, std::vector<int> shape) : matrix_shape(shape) {
         matrix_size = 1;
 
         for (int x : shape)
@@ -176,28 +44,120 @@ public:
 
         xmm_size = static_cast<unsigned long>(ceil(matrix_size / 8.0f));
 
-        unsigned long aligned_size = matrix_size / matrix_size % 8;
+        aligned_size = matrix_size / matrix_size % 8;
 
         for (int i = 0; i < aligned_size; i++) {
-            xmm.push_back(_mm256_loadu_ps(&vec[i * 8]));
+            xmm.push_back(_mm256_load_ps(&vec[i * 8]));
         }
 
         // Check for stranglers in case matrix size is not divisible by 8
-        int rem = static_cast<int>(matrix_size % 8);
-        if(rem) {
+        stranglers = static_cast<unsigned int>(matrix_size % 8);
+        if (stranglers) {
             // Set up a mask for partial loading.
             // Highest bit -> 1 in mask element means corresponding array element will be taken (i.e negative values)
             // Highest bit -> 0 in mask element means corresponding array element will be taken as 0
-            __m256i mask = _mm256_setr_epi32(-rem, 1-rem, 2-rem, 3-rem, 4-rem, 5-rem, 6-rem, 7-rem);
-            xmm.push_back(_mm256_maskload_ps(&vec[aligned_size*8], mask));
+            unsigned int rem = stranglers;
+            __m256i mask = _mm256_setr_epi32(-rem, 1 - rem, 2 - rem, 3 - rem, 4 - rem, 5 - rem, 6 - rem, 7 - rem);
+            xmm.push_back(_mm256_maskload_ps(&vec[aligned_size * 8], mask));
         }
     };
 
-    static void dot_product(MatrixAVX const &a, MatrixAVX &out) {
-//        __m256 c = _mm256_mul_ps(evens, odds);
+    explicit MatrixAVX(std::vector<int> shape) : matrix_shape(shape), aligned_size(0), stranglers(0) {
+        matrix_size = 1;
+
+        for (int x : shape)
+            matrix_size *= x;
+
+        xmm_size = static_cast<unsigned long>(ceil(matrix_size / 8.0f));
+
+
+        for (int i = 0; i < xmm_size; i++) {
+            xmm.push_back(_mm256_setzero_ps());
+        }
+    }
+
+    __m256 operator[](unsigned int index) const {
+        return getChunk(index);
+    }
+
+    float getElement(unsigned int index) {
+        _mm256_store_ps(aligned_float_arr, xmm[index / 8]);
+        return aligned_float_arr[index % 8];
+    }
+
+    void setElement(unsigned int index, float value) {
+        if (index >= matrix_size) {
+            throw std::out_of_range("Index " + to_string(index) + " is out of range. Matrix size is " +
+                                    to_string(matrix_size));
+        }
+        _mm256_store_ps(aligned_float_arr, xmm[index / 8]);
+        aligned_float_arr[index % 8] = value;
+        xmm[index / 8] = _mm256_load_ps(aligned_float_arr);
+    }
+
+    void setChunk(unsigned int index, __m256 chunk) {
+        if (index >= xmm_size) {
+            throw std::out_of_range(
+                    "Index " + to_string(index) + " is out of range. Total number of chunks is " + to_string(xmm_size));
+        }
+        xmm[index] = chunk;
+    }
+
+    __m256 getChunk(unsigned int index) const {
+        return xmm[index];
+    }
+
+    void add(const MatrixAVX &a, MatrixAVX &out) {
+        if (matrix_size != a.matrix_size) {
+            throw std::logic_error(
+                    "Matrices not of equal size (" + to_string(matrix_size) + ") vs (" + to_string(a.matrix_size) +
+                    ")");
+        }
+        for (unsigned int i = 0; i < xmm_size; i++) {
+            out.setChunk(i, _mm256_add_ps(xmm[i], a.xmm[i]));
+        }
+    }
+
+    void sub(const MatrixAVX &a, MatrixAVX &out) {
+        if (matrix_size != a.matrix_size) {
+            throw std::logic_error(
+                    "Matrices not of equal size (" + to_string(matrix_size) + ") vs (" + to_string(a.matrix_size) +
+                    ")");
+        }
+        for (unsigned int i = 0; i < xmm_size; i++) {
+            out.setChunk(i, _mm256_sub_ps(xmm[i], a.xmm[i]));
+        }
+    }
+    void dot_product(const MatrixAVX &a, MatrixAVX &out) {
+        if (matrix_size != a.matrix_size) {
+            throw std::logic_error("Matrices not of equal size");
+        }
+
+        std::fill(aligned_float_arr, aligned_float_arr + 8, 0);
+
+        unsigned int out_index = 0;
+
+        for (unsigned int i = 1; i <= xmm_size; i++) {
+            if (i % 8 == 0) {
+                out.setChunk(out_index++, _mm256_load_ps(aligned_float_arr));
+                std::fill(aligned_float_arr, aligned_float_arr + 8, 0);
+            }
+            aligned_float_arr[i - 1 % 8] = _mm256_cvtss_f32(hsums(_mm256_mul_ps(xmm[i - 1], a.xmm[i - 1])));
+        }
+        out.setChunk(out_index, _mm256_load_ps(aligned_float_arr));
+    }
+
+    friend std::ostream& operator<< (std::ostream& stream, const MatrixAVX& matrix) {
+        for(unsigned int i = 0; i < matrix.xmm_size; i++) {
+            stream << "[";
+            for(unsigned int j = 0; j < 7; j++)
+                stream << to_string(matrix.xmm[i][j]) + " ";
+            stream << to_string(matrix.xmm[i][7]);
+            stream << "]\n";
+        }
+
     }
 
 };
-
 
 #endif //INFERENCEENGINE_MATRIXAVX_H

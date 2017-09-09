59f0595eeb6b350c496ea74c012d766842134740
amrmorsey <amr.morsey@aucegypt.edu>
2017-09-09 21:28:38 +0200

    Removed comments

--- src/parsing_test.cpp
+++ src/parsing_test.cpp
@@ -10,17 +10,6 @@ using namespace std;
 int main() {
     Net net("simple_what.ahsf", "weights_original", "mean");
     Matrix image = net.loadMatrix("image_original", "image");
-//    MatrixAVX what({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.2}, {5, 2});
-//    MatrixAVX what2({1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 9.0, 10.2}, {2, 5});
-//    MatrixAVX out({2, 5});
-//    cout << what.getElement(9) << std::endl;
-//    what.dot_product(what2, out);
-//    std::cout << out;
-
-//    __m256 odds = _mm256_set_ps(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 3.0, 6.0);
-
-//    __m256 c =  _mm256_mul_ps(evens, odds);
-//    cout <<  _mm256_cvtss_f32(hsums(c));
     net.preprocess(image);
     net.setup(image);
     auto start = std::chrono::system_clock::now();
